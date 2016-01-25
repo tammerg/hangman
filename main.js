d469882428832322a5d64de7c2a7a95d846e4fe8
Tammer Galal <tg385@scarletmail.rutgers.edu>
2016-01-25 18:19:31 -0500

    working on getting main.js fixed and refactored. Closer to functionality

--- main.js
+++ main.js
@@ -6,6 +6,41 @@ var hangGame = {
   wordBank = ["jazzed", "beekeepers","parallaxes","grubbiness", "wobbliness","hobnobbing","fazes","wheeziness","mummifying"]
   guessesRemaining: 11
   currentWord: null
-  
+  startGame: function(Wrd){
+  var rando = Math.floor(Math.random() * 10)
+  this.currentWrd = new Word(this.wordBank[rando]);
+  this.currentWrd.getLets();
+  this.keepPromptingUser();
+  };
+  //gameplay/prompt function
+  keepPromptingUser: function(){
+    var self = this
+    prompt.get(["guessLetter"], function(err,result){
+      console.log("The Letter or space you guessed is" + result.guessLetter)
+      var findHowManyOfUserGuess = self.currentWrd.checkIfLetterFound(result.guessLetter)
+      if (findHowManyOfUserGuess === 0){
+        console.log("You guessed wrong!")
+        self.guessesRemaining--; 
+      }
+      else{
+        console.log("You guessed right!")
+        if (currentWord.didWeFindTheWrd() === true){
+          console.log("You Won!!!");
+          return 1;
+        }
+        else if(self.guessesRemaining === 0){
+          console.log("Game Over.");
+          console.log("The word was" + self.currentWrd.wrd);
+        }
+        else{
+          console.log(self.currentWrd.wordRender());
+        }
+      }
+    });
+  }
+};
+
+//starts the game
+hangGame.startGame();
 
 }
---
+++ word.js
@@ -0,0 +1,38 @@
+module.exports = Word;
+var Letter = require("./letter.js");
+
+function Word(wrd){
+  this.word = wrd;
+  this.lets = [];
+  this.found = false;
+  getLetters: function(){
+    for(var i = 0; i < word.length; i++){
+      this.lets.push(new Letter(wrd[i]));
+    };
+  };
+  checkIfLetterFound: function(guessLetter){
+    var lettersFound = 0;
+    for (var i = this.lets.length - 1; i >= 0; i--) {
+      if(this.lets[i].charac == guessLetter){
+        this.lets[i].appear = true;
+        lettersFound++;
+      };
+    }
+    return lettersFound;
+  };
+  didWeFindTheWOrd: function(){
+    if(this.lets.every(fucntion(curLet){
+      return curLet.appear === true;
+    })){
+      this.found === true
+    };
+    return this.found;
+  };
+  wordRender: function(){
+    var str = ""
+    for (var i = lets.length - 1; i >= 0; i--) {
+      str += this.lets[i].letterRender(this.lets[i]);
+    };
+    return str;
+  };
+};
