07c6747dafce7c30a69ed9050a21176bc25989ff
Tammer Galal <tg385@scarletmail.rutgers.edu>
2016-01-25 17:28:28 -0500

    began working on the javascript for the main.js file. Will continue building the games skeleton

---
+++ main.js
@@ -0,0 +1,11 @@
+var prompt = require ("prompt");
+prompt.start();
+var words = require("./word.js")
+
+var hangGame = {
+  wordBank = ["jazzed", "beekeepers","parallaxes","grubbiness", "wobbliness","hobnobbing","fazes","wheeziness","mummifying"]
+  guessesRemaining: 11
+  currentWord: null
+  
+
+}
